e77146c481f701e2662a6239edc5113631d52ee5
sachingadge-eai <102816872+sachingadge-eai@users.noreply.github.com>
2023-09-26 11:24:03 +0530

    update

--- pdfGenerator.js
+++ pdfGenerator.js
@@ -2,7 +2,7 @@ const express = require('express');
 var fs = require('fs')
 var conversion = require("phantom-html-to-pdf")();
 const app = express()
-const port = 3000;
+const port = 3001;
 const host = '0.0.0.0';
 
 let finalPdf = (link, name, res) => {
